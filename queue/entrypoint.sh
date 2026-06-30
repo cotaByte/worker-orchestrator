8e411fd17a63b195f757cc7fcf847da281a2aa40
cotaByte <minanajoverrafa@gmail.com>
2026-06-30 22:10:25 +0200

    fix/ securize the rabbitmq admin password by loading the definitions file template driven replacing env vars

---
+++ queue/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+: "${RABBITMQ_DEFAULT_USER:?RABBITMQ_DEFAULT_USER must be set}"
+: "${RABBITMQ_DEFAULT_PASS:?RABBITMQ_DEFAULT_PASS must be set}"
+
+RABBITMQ_PASSWORD_HASH=$(rabbitmqctl hash_password "$RABBITMQ_DEFAULT_PASS" | tail -n 1)
+
+sed \
+  -e "s|\${RABBITMQ_DEFAULT_USER}|$RABBITMQ_DEFAULT_USER|g" \
+  -e "s|\${RABBITMQ_PASSWORD_HASH}|$RABBITMQ_PASSWORD_HASH|g" \
+  /etc/rabbitmq/definitions.json.template > /etc/rabbitmq/definitions.json
+
+exec docker-entrypoint.sh "$@"
