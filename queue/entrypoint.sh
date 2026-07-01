39e366672193d14e70273698c22d883e534f8209
cotaByte <minanajoverrafa@gmail.com>
2026-07-01 22:45:48 +0200

    feat/ expose the port amqp to connect directly to the rabbitmq queue

--- queue/entrypoint.sh
+++ queue/entrypoint.sh
@@ -3,12 +3,17 @@ set -eu
 
 : "${RABBITMQ_DEFAULT_USER:?RABBITMQ_DEFAULT_USER must be set}"
 : "${RABBITMQ_DEFAULT_PASS:?RABBITMQ_DEFAULT_PASS must be set}"
+: "${RABBITMQ_API_USER:?RABBITMQ_API_USER must be set}"
+: "${RABBITMQ_API_PASS:?RABBITMQ_API_PASS must be set}"
 
 RABBITMQ_PASSWORD_HASH=$(rabbitmqctl hash_password "$RABBITMQ_DEFAULT_PASS" | tail -n 1)
+RABBITMQ_API_PASSWORD_HASH=$(rabbitmqctl hash_password "$RABBITMQ_API_PASS" | tail -n 1)
 
 sed \
   -e "s|\${RABBITMQ_DEFAULT_USER}|$RABBITMQ_DEFAULT_USER|g" \
   -e "s|\${RABBITMQ_PASSWORD_HASH}|$RABBITMQ_PASSWORD_HASH|g" \
+  -e "s|\${RABBITMQ_API_USER}|$RABBITMQ_API_USER|g" \
+  -e "s|\${RABBITMQ_API_PASSWORD_HASH}|$RABBITMQ_API_PASSWORD_HASH|g" \
   /etc/rabbitmq/definitions.json.template > /etc/rabbitmq/definitions.json
 
 exec docker-entrypoint.sh "$@"
